c3f5de59dfe3d11efa474441e1dfb04ea7c60072
Kendrick Taylor <sktayloriii@gmail.com>
2010-06-06 12:21:17 -0400

    Added new jacksons, added skills to server.js

---
+++ JacksonSkills.js
@@ -0,0 +1,15 @@
+var sys = require("sys");
+var mp = require("./MessageProcessor");
+var riskDT = require("./BuildingSurveyDecisionTree");
+
+function main() {
+
+    sys.puts("Server Started.");
+
+    var survey = new mp.MessageProcessor("skills", new riskDT.BuildingSurveyDecisionTree());
+
+    survey.Start();
+}
+
+
+main();
