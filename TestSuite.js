9aab8745e003e616c70dc9e1828249194d57379d
Kendrick Taylor <sktayloriii@gmail.com>
2010-06-06 21:25:54 -0400

    added test suite.

--- TestSuite.js
+++ TestSuite.js
@@ -1,8 +1,13 @@
+var sys = require('sys');
 var qresp = new (require("./TestQuestionResponse")).TestQuestionResponse;
 
 function main() {
 
+    sys.puts("\n-- START -- Question Response Tests ---------------------------------\n");
+
     qresp.RunTests();
+
+    sys.puts("\n-- END ---- Question Response Tests ---------------------------------\n");
 }
 
 
